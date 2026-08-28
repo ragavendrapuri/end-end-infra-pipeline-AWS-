7023fd9bf83e365330de014101a3bce5dd534290
Raghavendra puri <124024434+ragavendrapuri@users.noreply.github.com>
2026-08-28 13:56:21 +0530

    fix: resolve subnet CIDR conflict and S3 bucket naming

--- modules/storage.tf
+++ modules/storage.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "${var.cloud_env}-N10134-${var.bucket_name}"
+  bucket = lower("${var.cloud_env}-${var.bucket_name}-${random_id.random.hex}")
 }
 
 resource "aws_s3_bucket_versioning" "s3_versioning" {
